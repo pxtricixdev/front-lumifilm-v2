f9f591799a93cbd15da86c24adff5f69e7395b6e
Patricia <patricia.abadaranda@gmail.com>
2025-01-25 17:08:09 +0100

    add background color

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,5 +1,24 @@
 <template>
-  <header><h1>Esto será el header</h1></header>
+  <div style="position: fixed; z-index: -4; top: 0px; left: 0px; height: 100%; width: 100%">
+    <div
+      style="
+        position: absolute;
+        top: 0;
+        z-index: -1;
+        height: 100%;
+        width: 100%;
+        background: radial-gradient(
+            ellipse 80% 80% at 50% -20%,
+            rgba(120, 119, 198, 0.3),
+            rgba(255, 255, 255, 0)
+          ),
+          #0a0a0a;
+      "
+    ></div>
+  </div>
+  <header>
+    <MainHeader />
+  </header>
   <main>
     <RouterView />
   </main>
