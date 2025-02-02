c35f9b755c8931f4494d8d5499118645fbe1b2dd
Patricia <patricia.abadaranda@gmail.com>
2025-02-02 12:19:15 +0100

    add type and size props

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,31 +1,56 @@
 <template>
-  <button :type="type" :disabled="disabled" class="button">{{ label }}</button>
+  <button :type="type" :disabled="disabled" :class="['button', size]">
+    {{ label }}
+  </button>
 </template>
 
 <script setup lang="ts">
 defineProps<{
   label: string
-  type: string
+  type: 'button' | 'submit' | 'reset'
+  size: 'small' | 'medium' | 'large'
   disabled?: boolean
 }>()
 </script>
 
 <style lang="scss" scoped>
-@use '../assets/styles/variables' as *;
-@use '../assets/styles/mixins' as *;
+@use '@/assets/styles/variables' as *;
+@use '@/assets/styles/mixins' as *;
 
 .button {
-  @include semibold-text(16px);
   background-color: $blue-color;
   color: $white-color;
   border: none;
   border-radius: 15px;
   text-align: center;
   cursor: pointer;
-}
 
-.button:hover {
-  opacity: 0.9;
-  transition: ease-in 0.1s;
+  &:hover {
+    opacity: 0.9;
+    transition: ease-in 0.1s;
+  }
+
+  &:disabled {
+    background-color: #2b2b2b;
+    cursor: not-allowed;
+  }
+
+  &.small {
+    padding: 8px 25px;
+    @include semibold-text(12px);
+    height: 40px;
+  }
+
+  &.medium {
+    padding: 12px 40px;
+    @include semibold-text(16px);
+    height: 45px;
+  }
+
+  &.large {
+    padding: 14px 60px;
+    @include semibold-text(18px);
+    height: 50px;
+  }
 }
 </style>
