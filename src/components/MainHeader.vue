6ee4cdde70447326c99324a196054f9046646595
Patricia <patricia.abadaranda@gmail.com>
2025-02-02 12:17:06 +0100

    change of import scss

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -20,8 +20,8 @@
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped>
-@use '../assets/styles/variables' as *;
-@use '../assets/styles/mixins' as *;
+@use '@/assets/styles/variables' as *;
+@use '@/assets/styles/mixins' as *;
 @import url('https://fonts.googleapis.com/css2?family=Darumadrop+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Unbounded:wght@200..900&display=swap');
 
 .header {
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <h2>Contenido principal de Home</h2>
 </template>
+
 <script lang="ts" setup></script>
 
 <style lang="scss" scoped>
-@use '../assets/styles/variables' as *;
-@use '../assets/styles/mixins' as *;
-
+@use '@/assets/styles/variables' as *;
+@use '@/assets/styles/mixins' as *;
 </style>
