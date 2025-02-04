3b8522c5084ca7949aec704c177909e8aaf17763
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-02-04 19:50:33 +0100

    add seat icon

---
+++ src/assets/icons/SeatIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg width="140" height="160" viewBox="0 0 300 320" xmlns="http://www.w3.org/2000/svg">
+    <!-- Respaldo superior -->
+    <path d="M60 50 Q150 0 240 50 V143 H60 Z" :fill="fill" :stroke="stroke" />
+
+    <!-- Asiento -->
+    <rect x="50" y="150" width="200" height="80" rx="20" ry="20" :fill="fill" :stroke="stroke" />
+
+    <!-- Patas -->
+    <rect x="70" y="230" width="40" height="50" rx="5" ry="5" :fill="fill" :stroke="stroke" />
+    <rect x="190" y="230" width="40" height="50" rx="5" ry="5" :fill="fill" :stroke="stroke" />
+
+    <!-- Brazos de la butaca -->
+    <rect x="30" y="120" width="40" height="90" rx="20" ry="20" :fill="fill" :stroke="stroke" />
+    <rect x="230" y="120" width="40" height="90" rx="20" ry="20" :fill="fill" :stroke="stroke" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  stroke: string
+  fill?: string
+}>()
+</script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,20 @@
 <template>
-  <h2>Contenido principal de Home</h2>
+  <main class="main__container">
+    <h2>SALA DE CINE</h2>
+  </main>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+</script>
 
 <style lang="scss" scoped>
 @use '@/assets/styles/variables' as *;
 @use '@/assets/styles/mixins' as *;
+
+.main__container {
+  font-weight: 400;
+  color: white;
+  padding-top: 50px;
+  text-align: center;
+}
 </style>
